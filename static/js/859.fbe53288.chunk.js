d6763f4ee871928b20313add61f3aeb40c4b6780
ivanpanteliuk <113294026+ivanpanteliuk@users.noreply.github.com>
2023-06-05 19:56:42 +0000

    Deploying to gh-pages from @ ivanpanteliuk/goit-test-app@d487ed0f679dc2429ad452783b5a84d16211a973 🚀

--- static/js/859.fbe53288.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_test_app=self.webpackChunkgoit_test_app||[]).push([[859],{4859:function(t,e,n){n.r(e),n.d(e,{default:function(){return i}});var u=n(184);function i(){return(0,u.jsx)("div",{children:"Home"})}}}]);
-//# sourceMappingURL=859.fbe53288.chunk.js.map
