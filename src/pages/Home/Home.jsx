1fa43f8e7152e1c51df0bfe14ea9c28ed4ce091e
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2023-06-05 21:48:48 +0200

    add home page styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,18 @@
+import { Section } from "../../components/App/App.styled";
+import { HeroContainer, HeroSubText, HeroText } from "./Home.styled";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Section>
+      <HeroContainer>
+        <HeroText>Tweets</HeroText>
+        <HeroSubText>
+          Welcome to Tweets, a social media platform where you can stay
+          connected with your favorite influencers and stay updated with their
+          tweets. Follow your favorite influencers, interact with their content,
+          and engage with a vibrant community of users.
+        </HeroSubText>
+      </HeroContainer>
+    </Section>
+  );
 }
---
+++ src/pages/Home/Home.styled.js
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+import follow from "../../images/Follow.png";
+import following from "../../images/Following.png";
+import sms from "../../images/sms.png";
+
+export const HeroContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0px 15px;
+  width: 1280px;
+  height: 550px;
+  text-align: center;
+
+  background-image: url(${sms});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 900px 500px;
+`;
+
+export const HeroText = styled.h1`
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: 30px;
+
+  font-weight: 900;
+  font-size: 30px;
+  line-height: 1.6153;
+  text-transform: uppercase;
+  letter-spacing: 0.06em;
+`;
+
+export const HeroSubText = styled.p`
+  position: absolute;
+  max-width: 350px;
+  text-align: center;
+  border: 1px solid black;
+  padding: 10px;
+  border-radius: 10px;
+  right: 260px;
+  top: 223px;
+`;
